586722a0fce00ea005a6b3b4208ad0bd79436999
Jérémie Posé <j.pose@thecodingmachine.com>
2014-06-05 16:05:51 +0200

    add JqueryUploadSingleFile Renderer and descriptor

---
+++ src/Mouf/MVC/BCE/Classes/Descriptors/JqueryUploadSingleFileFieldDescriptor.php
@@ -0,0 +1,181 @@
+<?php
+namespace Mouf\MVC\BCE\Classes\Descriptors;
+use Mouf\Html\Widgets\JqueryFileUpload\JqueryFileUploadWidget;
+use Mouf\MVC\BCE\BCEForm;
+use Mouf\MVC\BCE\Classes\BCEException;
+
+/**
+ * This class is the simpliest FieldDescriptor:
+ * it handles a field that has no "connections" to other objects (
+ * as user name or login for example)
+ * @Component
+ */
+class JqueryUploadSingleFileFieldDescriptor extends FieldDescriptor {
+
+    /**
+     * The name of the function that returns the value of the field from the bean.
+     * For example, with $user->getLogin(), the $getter property should be "getLogin"
+     * @Property
+     * @var string
+     */
+    private $fullPathGetter;
+
+    /**
+     * The name of the function that sets the value of the field into the bean.
+     * For example, with $user->setLogin($login), the $setter property should be "setLogin"
+     * @Property
+     * @var string
+     */
+    private $fileNameSetter;
+
+    /**
+     *
+     * @var JqueryFileUploadWidget
+     */
+    protected $fileUploaderWidget;
+
+    /**
+     * The value of the field once the FiedDescriptor has been loaded
+     * @var mixed
+     */
+    protected $value;
+
+
+    /**
+     * @param string $fullPathGetter The name of the function that returns the full path to the file.
+     * @param string $fileNameSetter The name of the function that sets the name of the file in the bean.
+     */
+    public function __construct($fullPathGetter, $fileNameSetter, JqueryFileUploadWidget $fileUploaderWidget) {
+        $this->fullPathGetter = $fullPathGetter;
+        $this->fileNameSetter = $fileNameSetter;
+        $this->fileUploaderWidget = $fileUploaderWidget;
+    }
+
+    /**
+     * Loads the values of the bean into the descriptors, calling main bean's getter
+     * Eventually formats the value before displaying it
+     * @param mixed $mainBean
+     */
+    public function load($mainBean, $id = null, &$form = null) {
+        $fieldValue = $this->getValue($mainBean);
+        $descriptorInstance = new FieldDescriptorInstance($this, $form, $id);
+
+        $descriptorInstance->value = $fieldValue;
+        $this->fileUploaderWidget->setMaxNumberOfFiles(1);
+        return $descriptorInstance;
+    }
+
+
+    /**
+     * (non-PHPdoc)
+     * For a FieldDecsriptor instance, the preSave function id responsible for :
+     *  - unformatting the posted value
+     *  - valideting the value
+     *  - setting the value into the bean (case of BaseFieldDescriptors)
+     *  - settings the linked ids to associate in mapping table (Many2ManyFieldDEscriptors)
+     * @see BCEFieldDescriptorInterface::preSave()
+     */
+    public function preSave($post, BCEForm &$form, $bean, $isNew) {}
+
+
+    /**
+     * (non-PHPdoc)
+     * @see BCEFieldDescriptorInterface::postSave()
+     */
+    public function postSave($bean, $beanId, $postValues) {
+
+        $fullPath = call_user_func(array($bean, $this->fullPathGetter));
+
+        $this->fileUploaderWidget->setName($this->getFieldName());
+
+        // Retrieve post to check if the user deletes a file
+
+        // Let's manage removes.
+        // First, let's get the name of the remove field.
+        $deleteName = $this->fileUploaderWidget->getDeleteName();
+
+        if($postValues != null) {
+            $removes = isset($postValues[$deleteName]) ? $postValues[$deleteName] : null;
+        } else {
+            $removes = get($deleteName);
+        }
+        if($removes) {
+
+           foreach ($removes as $fileMd5) {
+                if (md5($fullPath) == $fileMd5) {
+                    $result = unlink($fullPath);
+                    if (!$result) {
+                        throw new BCEException("Unable to delete file ".$fullPath);
+                    }
+                    call_user_func(array($bean, $this->fileNameSetter), null);
+                }
+            }
+
+        }
+
+        foreach ($this->fileUploaderWidget->getFiles($postValues[$this->getFieldName()]) as $file) {
+            call_user_func(array($bean, $this->fileNameSetter), $file->getFileName());
+            $fullPath2 = call_user_func(array($bean, $this->fullPathGetter));
+
+            // Let's check that the file has not been deleted just after being uploaded.
+            if($removes) {
+                $abort = false;
+
+                foreach ($removes as $fileMd5) {
+                    if ($fileMd5 == md5($fullPath2)) {
+                        $file->delete();
+                        call_user_func(array($bean, $this->fileNameSetter), null);
+                        $abort = true;
+                        break;
+                    }
+                }
+                if ($abort) {
+                    continue;
+                }
+            }
+
+            $file->moveAndRename(dirname($fullPath2), basename($fullPath2));
+        }
+    }
+
+    /**
+     * Simply calls the setter of the descriptor's related field into the bean
+     * @param mixed $mainBean
+     * @param mixed $value
+     */
+    public function setValue($mainBean, $value) {
+        call_user_func(array($mainBean, $this->fileNameSetter), $value);
+    }
+
+    public function getValue($mainBean){
+        if ($mainBean == null){
+            $fieldValue = null;
+        }else{
+            $fieldValue = call_user_func(array($mainBean, $this->fullPathGetter));
+        }
+        return $fieldValue;
+    }
+
+
+    /**
+     * Returns the bean's value after loading the descriptor
+     */
+    public function getFieldValue() {
+        return $this->value;
+    }
+
+    /**
+     * Returns the label of the field
+     */
+    public function getFieldLabel() {
+        return $this->label;
+    }
+
+    /**
+     * Returns the instance of FileUploaderWidget
+     */
+    public function getFileUploaderWidget() {
+        return $this->fileUploaderWidget;
+    }
+
+}
---
+++ src/Mouf/MVC/BCE/Classes/Renderers/JqueryUploadSingleFileRenderer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Base class for rendering tree select field
+ */
+
+namespace Mouf\MVC\BCE\Classes\Renderers;
+
+use Mouf\MVC\BCE\Classes\Descriptors\FieldDescriptorInstance;
+use Mouf\Html\Widgets\Form;
+use Mouf\MVC\BCE\Classes\Descriptors\JqueryUploadSingleFileFieldDescriptor;
+use Mouf\MVC\BCE\Classes\ValidationHandlers\BCEValidationUtils;
+use Mouf\MVC\BCE\Classes\BCEException;
+use Mouf\Html\Widgets\JqueryFileUpload\FileWidget;
+use Mouf\Html\Widgets\JqueryFileUpload\JqueryFileUploadField;
+
+class JqueryUploadSingleFileRenderer extends DefaultViewFieldRenderer implements SingleFieldRendererInterface {
+
+    /**
+     * (non-PHPdoc)
+     * @see FieldRendererInterface::render()
+     */
+    public function renderEdit($descriptorInstance){
+    	/* @var $descriptorInstance FieldDescriptorInstance */
+    	
+        $descriptor = $descriptorInstance->fieldDescriptor;
+
+        if (!$descriptor instanceof JqueryUploadSingleFileFieldDescriptor) {
+        	throw new BCEException("You can only use JqueryUploadMultiFileRenderer on instances of JqueryUploadMultiFileFieldDescriptor");
+        }
+        
+        $fileUploadWidget = $descriptor->getFileUploaderWidget();
+        
+        $fileUploadWidget->setName($descriptor->getFieldName());
+        if($descriptorInstance->getFieldValue() != null){
+            $fileUploadWidget->addDefaultFile(new FileWidget($descriptorInstance->getFieldValue(), md5($descriptorInstance->getFieldValue())));
+        }
+
+        $fileUploadField = new JqueryFileUploadField($descriptor->getFieldLabel());
+        $fileUploadField->setRequired(BCEValidationUtils::hasRequiredValidator($descriptorInstance->fieldDescriptor->getValidators()));
+        $fileUploadField->setJqueryFileUploadWidget($fileUploadWidget);
+        
+        ob_start();
+        $fileUploadField->toHtml();
+        return ob_get_clean();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see FieldRendererInterface::getJS()
+     */
+    public function getJSEdit($descriptor, $bean, $id){
+        /* @var $descriptorInstance FieldDescriptorInstance */
+        return array();
+    }
+}
